e7d6a0c7fdbc33b2c2e75db591718c1ae7ba1963
Taylor Cool <taycool@gmail.com>
2019-10-18 08:43:55 -0700

    finished objects

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,29 +6,54 @@
   object name, diet, weight, length, period
 */
 
+
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+const tyran = {
+  name: 'tyrannosaurus',
+  diet: 'carniverous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceous',
+  roar: function(){
+    return "RAWERSRARARWERSARARARRRR!";
+  }
+};
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+const stego = {
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic'
+};
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+const veloc = {
+  name: 'velociraptor',
+  diet: 'carniverous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous'
+};
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyran.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(veloc.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stego.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyran.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyran.roar());
 
 
 // ==== Arrays ====
@@ -51,7 +76,8 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
+const universities = graduates.map(a => a.university);
+universities.sort();
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
